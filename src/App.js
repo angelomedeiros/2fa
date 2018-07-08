a1b42fd0f795b11bfbc73a6ba04c14ddabea8a4c
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-08 14:05:39 -0300

    Add o input para inserir o 2fa

--- src/App.js
+++ src/App.js
@@ -9,17 +9,18 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      secret_tmp: JSON.parse(localStorage.getItem('2fa_tmp')) || {}
+      secret_tmp: JSON.parse(localStorage.getItem('2fa_tmp')) || {},
+      token: ''
     }
     this.gerarQrCode = this.gerarQrCode.bind(this)
     this.verificaToken = this.verificaToken.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentWillMount() {    
     if ( !localStorage.getItem('2fa_tmp') ) {
       var secret = speakeasy.generateSecret();
-      localStorage.setItem( '2fa_tmp', JSON.stringify(secret) )    
-      console.log('Teste')  
+      localStorage.setItem( '2fa_tmp', JSON.stringify(secret) )
     }
 
     this.setState({
@@ -46,6 +47,12 @@ class App extends Component {
     return verified
   }
 
+  handleChange (event) {
+    this.setState({
+      token: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -59,7 +66,12 @@ class App extends Component {
         <div>
           <h2>Testando o 2fa com o speakeasy</h2>
           <img src={`${this.gerarQrCode()}`} />
-          <h3>{console.log(this.verificaToken('931104'))}</h3>
+          <p> Capture o código QR no app de autenticação </p>
+          <label htmlFor="token">Insira o código: </label>
+          <input type='text' id='token' value={this.state.token} onChange={this.handleChange} />
+          <h3>{
+            this.verificaToken(this.state.token) ? 'Sucesso, código válido!' : 'Código inválido, obtenha um novo!'
+          }</h3>
         </div>
       </div>
     );
